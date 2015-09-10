f5c6e6d90e1cece4b61c2fc544baf25ec4befb48
James Lopez <James.a0.lopez@gmail.com>
2015-09-10 14:48:57 -0500

    Gruntfile.js and package.json dependencies updated.

--- gruntfile.js
+++ gruntfile.js
@@ -11,32 +11,31 @@ module.exports = function(grunt){
 				}
 			}
 		},
-		watch: {
-			css:{
-				files: '**/*.scss',
-				tasks: ['sass']
-			}
-		},
 		copy: {
 			build: {
 				cwd: 'source',
-				src: ['**', '!**/*.styl'],
+				src: ['**', '!**/*.styl', '!**/*.coffee'],
 				dest: 'build',
 				expand: true
-
 			}
 		},
 		clean: {
 			build:{
 				src: ['build']
-			}
+			},
+			stylesheets : {
+				src: ['build/**/*.css', '!build/application.css']
+			},
+			scripts : {
+				src: ['build/**/*.js', '!build/application.js']
+			},
 		},
 		stylus: {
 			build: {
 				options: {
 					linenos: true,
 					compress: false
-				};
+				},
 			files: [{
 				expand: true,
 				cwd: 'source',
@@ -61,9 +60,54 @@ module.exports = function(grunt){
 				}
 			}
 		},
+		coffee: {
+			build: {
+				expand: true,
+				cwd: 'source',
+				src: [ '**/*.coffee' ],
+				dest: 'build',
+				ext: '.js'
+			}
+		},
+		uglify: {
+			build: {
+				options: {
+					mangle: false
+				},
+				files: {
+					'build/application.js': ['build/**/*.js']
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			},
+			stylesheets: {
+				files: 'source/**/*.styl',
+				tasks: ['stylesheets']
+			},
+			scripts: {
+				files: 'source/**/*.coffee',
+				tasks: ['scripts']
+			},
+			copy: {
+				files: ['source/**', '!source/**/*.styl', '!source/**/*.coffee'],
+				tasks: ['copy']
+			},
+		},
+		connect: {
+			server: {
+				options: {
+					port: 4000,
+					base: 'build',
+					hostname: 'localhost'
+				}
+			}
+		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
 	//Copies files from source directory to build
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	//wipes build directory clean
@@ -71,10 +115,18 @@ module.exports = function(grunt){
 	//adds vendor previxes to CSS3 properties AFTER stylus files compliled to CSS.
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	//Minifies CSS files and combines them into a single file
-	grunt.loadNpmTasks('grunt-contrib-cssmin')
-	grunt.registerTask('default',['watch']);
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	//Minifies Javascript files and combines into 1 file
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	//Watches source code for changes and auto builds them
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	//Watches the project for chanes, automatically builds them and runs a server
+	grunt.registerTask('default', ['build', 'connect', 'watch']);
 	//Compiles the stylesheets
-	grunt.registerTask('stylesheets', [ 'stylus', 'autoprefixer', 'cssmin' ]);
+	grunt.registerTask('stylesheets', [ 'stylus', 'autoprefixer', 'cssmin', 'clean:stylesheets' ]);
+	//Compiles the Javascript files.
+	grunt.registerTask('scripts', ['coffee', 'uglify', 'clean:scripts']);
 	//Compiles all of the assets and copies the files to the build directory.
-	grunt.registerTask('build',['clean', 'copy','stylesheets']);
-}
+	grunt.registerTask('build', ['clean:build', 'copy','stylesheets', 'scripts']);
+};
